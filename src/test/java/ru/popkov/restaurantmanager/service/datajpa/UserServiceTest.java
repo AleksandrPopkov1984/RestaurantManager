60d4bb6d4e6dd7af956b73a2ff0eee6959b8170e
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-12-19 08:36:41 +0300

    add CacheManager clear to UserServiceTest

--- src/test/java/ru/popkov/restaurantmanager/service/datajpa/UserServiceTest.java
+++ src/test/java/ru/popkov/restaurantmanager/service/datajpa/UserServiceTest.java
@@ -1,7 +1,9 @@
 package ru.popkov.restaurantmanager.service.datajpa;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.dao.DataAccessException;
 import ru.popkov.restaurantmanager.model.User;
 import ru.popkov.restaurantmanager.service.AbstractServiceTest;
@@ -17,6 +19,14 @@ public class UserServiceTest extends AbstractServiceTest {
 
     @Autowired
     private UserService service;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @BeforeEach
+    void setup() {
+        cacheManager.getCache("users").clear();
+    }
 
     @Test
     public void get() {
