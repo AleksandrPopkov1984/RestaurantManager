7b4dce28e541082a52e990d6bd53d486200d6647
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-12-22 12:09:19 +0300

    add model object Menu

---
+++ src/main/java/ru/popkov/restaurantmanager/model/Menu.java
@@ -0,0 +1,63 @@
+package ru.popkov.restaurantmanager.model;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "menus")
+public class Menu extends AbstractBaseEntity {
+
+    @Column(name = "date", nullable = false)
+    @NotNull
+    private LocalDate date;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
+    private Restaurant restaurant;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "menu")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
+    private List<Meal> meals;
+
+    public Menu() {
+    }
+
+    public Menu(Integer id, LocalDate date, Restaurant restaurant, List<Meal> meals) {
+        super(id);
+        this.date = date;
+        this.restaurant = restaurant;
+        this.meals = meals;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+}
