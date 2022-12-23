5eb408514173e000ee3364e8ffe6275fdc47303b
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-12-23 10:06:09 +0300

    add MenusUtil

---
+++ src/main/java/ru/popkov/restaurantmanager/util/MenusUtil.java
@@ -0,0 +1,22 @@
+package ru.popkov.restaurantmanager.util;
+
+import ru.popkov.restaurantmanager.model.Menu;
+import ru.popkov.restaurantmanager.to.MenuTo;
+
+import java.util.List;
+
+public class MenusUtil {
+
+    private MenusUtil() {
+    }
+
+    public static List<MenuTo> getTos(List<Menu> menus) {
+        return menus.stream()
+                .map(MenusUtil::createTo)
+                .toList();
+    }
+
+    public static MenuTo createTo(Menu menu) {
+        return new MenuTo(menu.getId(), menu.getDate(), menu.getRestaurant(), menu.getMeals());
+    }
+}
