7397e18f455f58e89d0ebd48823932ee419813cb
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-12-30 14:42:31 +0300

    add class RestaurantUserUIControllerTest with testing getAll method

--- src/main/java/ru/popkov/restaurantmanager/web/restaurant/RestaurantUserUIController.java
+++ src/main/java/ru/popkov/restaurantmanager/web/restaurant/RestaurantUserUIController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.*;
 import ru.popkov.restaurantmanager.model.Restaurant;
 
 @Controller
-@RequestMapping("/user/restaurants")
+@RequestMapping(RestaurantUserUIController.URL)
 public class RestaurantUserUIController extends AbstractRestaurantController {
+
+    public static final String URL = "/user/restaurants";
 
     @GetMapping
     public String getAll(Model model) {
