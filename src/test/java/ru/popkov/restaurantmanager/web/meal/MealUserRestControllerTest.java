9dabd2bc943c013e511bb8b4de9d0383bdce9b3f
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-12-31 10:17:45 +0300

    implement MealUserRestController with tests

---
+++ src/test/java/ru/popkov/restaurantmanager/web/meal/MealUserRestControllerTest.java
@@ -0,0 +1,45 @@
+package ru.popkov.restaurantmanager.web.meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.popkov.restaurantmanager.web.AbstractControllerTest;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static ru.popkov.restaurantmanager.MenuTestData.MENU1_ID;
+import static ru.popkov.restaurantmanager.web.meal.MealUserRestController.REST_URL;
+import static ru.popkov.restaurantmanager.MealTestData.*;
+
+class MealUserRestControllerTest extends AbstractControllerTest {
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/" + MEAL1_ID))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MEAL_TO_MATCHER.contentJson(mealTo1));
+    }
+
+    @Test
+    void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/" + NOT_FOUND))
+                .andDo(print())
+                .andExpect(view().name("exception/exception"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/exception/exception.jsp"));
+    }
+
+    @Test
+    void getOfMenu() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/ofMenu")
+                .param("menuId", String.valueOf(MENU1_ID)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MEAL_TO_MATCHER.contentJson(List.of(mealTo1, mealTo2)));
+    }
+}
