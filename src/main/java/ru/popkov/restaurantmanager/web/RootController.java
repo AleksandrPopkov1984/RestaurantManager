d451d1ca468f8a5b6ea64fa1d763e2af2f347660
Alexandr Popkov <alekspopkov1984@gmail.com>
2023-01-02 23:35:39 +0300

    correct start page for Admin and User

--- src/main/java/ru/popkov/restaurantmanager/web/RootController.java
+++ src/main/java/ru/popkov/restaurantmanager/web/RootController.java
@@ -2,26 +2,35 @@ package ru.popkov.restaurantmanager.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import ru.popkov.restaurantmanager.model.Role;
+import ru.popkov.restaurantmanager.model.User;
+import ru.popkov.restaurantmanager.service.UserService;
+
+import java.util.Objects;
 
 @Controller
 public class RootController {
 
     private static final Logger log = LoggerFactory.getLogger(RootController.class);
 
+    @Autowired
+    private UserService service;
+
     @GetMapping("/")
     public String root() {
-        log.info("root");
-        return "index";
-    }
-
-    @PostMapping("/start")
-    public String redirect(@RequestParam String userType) {
-        log.info("redirect to {}", userType);
-        return "redirect:" + userType;
+        int userId = Objects.requireNonNull(SecurityUtil.safeGet()).getId();
+        User authorizedUser = service.get(userId);
+        boolean hasAdminRole = authorizedUser.getRoles().contains(Role.ADMIN);
+        if (hasAdminRole) {
+            log.info("admin root");
+            return "admins";
+        } else {
+            log.info("user root");
+            return "redirect:user/restaurants";
+        }
     }
 
     @GetMapping("/login")
--- src/test/java/ru/popkov/restaurantmanager/web/RootControllerTest.java
+++ src/test/java/ru/popkov/restaurantmanager/web/RootControllerTest.java
@@ -6,18 +6,29 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.popkov.restaurantmanager.TestUtil.userAuth;
+import static ru.popkov.restaurantmanager.UserTestData.admin;
 import static ru.popkov.restaurantmanager.UserTestData.user;
 
 public class RootControllerTest extends AbstractControllerTest {
 
     @Test
-    void getIndexPage() throws Exception {
+    void getAdminStartPage() throws Exception {
+        perform(get("/")
+                .with(userAuth(admin)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("admins"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/admins.jsp"));
+    }
+
+    @Test
+    void getUserStartPage() throws Exception {
         perform(get("/")
                 .with(userAuth(user)))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/index.jsp"));
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:user/restaurants"))
+                .andExpect(redirectedUrl("user/restaurants"));
     }
 
     @Test
